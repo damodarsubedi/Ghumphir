19f18c4ea2897170ff64abe23bcfaa6172d4a6c7
sri123jan <srijan.katuwal@gmail.com>
2020-06-29 08:53:44 +0545

    General auth

--- controller/route.js
+++ controller/route.js
@@ -1,8 +1,22 @@
 
 
 var route = function(app){
+    
     app.get('/', (req, res) => {
         res.render('home');
+    });
+
+    app.post('/login', (req, res) => {
+        
+        res.send('Login');
+    });
+    app.get('/signup', (req, res) => {
+        
+        res.render('signup');
+    });
+    app.post('/signup', (req, res) => {
+        console.log(req.body); 
+        res.render('signup');
     });
 }
 
